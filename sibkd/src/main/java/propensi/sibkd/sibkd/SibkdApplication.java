38370005819a0c1fcbca6dff3fd4b2527549f66d
shafirazzhr <firazzahra2@gmail.com>
2022-02-26 17:09:40 +0700

    add project

---
+++ sibkd/src/main/java/propensi/sibkd/sibkd/SibkdApplication.java
@@ -0,0 +1,13 @@
+package propensi.sibkd.sibkd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SibkdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SibkdApplication.class, args);
+	}
+
+}
---
+++ sibkd/src/test/java/propensi/sibkd/sibkd/SibkdApplicationTests.java
@@ -0,0 +1,13 @@
+package propensi.sibkd.sibkd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SibkdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
