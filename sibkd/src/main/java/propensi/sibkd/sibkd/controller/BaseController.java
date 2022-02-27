dbf199fcb4f8bf2ebb49700c6e7f462579ed09bd
shafirazzhr <firazzahra2@gmail.com>
2022-02-27 22:25:01 +0700

    add css, js, navbar, lp

---
+++ sibkd/src/main/java/propensi/sibkd/sibkd/controller/BaseController.java
@@ -0,0 +1,11 @@
+package propensi.sibkd.sibkd.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BaseController {
+    @GetMapping("/")
+    private String home(){
+        return"home";
+    }
+}
